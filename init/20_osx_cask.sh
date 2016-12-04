e2386639b69b9f5667ce2899114e1e7925d2c8e5
Jan Michael Auer <mail@jauer.org>
2016-12-04 15:00:52 +0100

    Clean out brew casks.

--- init/20_osx_cask.sh
+++ init/20_osx_cask.sh
@@ -4,29 +4,18 @@ is_osx || return 0
 	# Casks to install
 	casks=(
 		1password
-		android
 		atom
-		clion
-		djview
 		dropbox
-		evernote
 		firefox
 		flux
 		flycut
-		goofy
-		hipchat
-		inkscape
+		google-chrome
 		insync
-		intellij-idea
 		intel-haxm
-		istumbler
 		iterm2
 		java
-		jdownloader
 		mou
 		osxfuse
-		phpstorm
-		pycharm
 		qlcolorcode
 		qlimagesize
 		qlmarkdown
@@ -34,13 +23,10 @@ is_osx || return 0
 		quicklook-csv
 		quicklook-json
 		skype
-		speak
 		spotify
 		teamviewer
 		texpad
-		vagrant
 		vlc
-		webstorm
 	)
 
 	# Install the casks
