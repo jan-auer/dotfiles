f4da646350655527208e65633878cc3c438752d7
Jan Michael Auer <mail@jauer.org>
2018-12-29 14:32:26 +0100

    Account for missing chsh in installer

--- init/30_zsh.sh
+++ init/30_zsh.sh
@@ -1,4 +1,13 @@
 if [[ ! "$SHELL" == *zsh ]] ; then
-	chsh -s $(which zsh)
+	if (( $+commands[chsh] )); then
+		chsh -s $(which zsh)
+	else
+		echo "Could not chsh. Add this to your .profile:"
+		echo
+		echo "if [[ -x /usr/local/bin/zsh ]]; then"
+		echo "	export SHELL=/usr/local/bin/zsh"
+		echo "	exec /usr/local/bin/zsh"
+		echo "fi"
+		echo
+	fi
 fi
-
