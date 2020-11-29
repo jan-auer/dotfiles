182febda1dd9c9a3125f578038863b0dbfef4d1a
Jan Michael Auer <mail@jauer.org>
2020-11-29 13:57:52 +0100

    Switch iTerm color preset when system setting changes

--- init/40_defaults.sh
+++ init/40_defaults.sh
@@ -1,9 +1,19 @@
 # VSCode settings
 
 if ( is_osx ); then
-  mkdir -p "$HOME/Library/Application Support/Code/User"
-  ln -sf "$ROOT/defaults/vscode.json" "$HOME/Library/Application Support/Code/User/settings.json"
+  VSCODE_DIR="$HOME/Library/Application Support/Code/User"
 elif ( is_linux ); then
-  mkdir -p "$HOME/.config/Code/User"
-  ln -sf "$ROOT/defaults/vscode.json" "$HOME/.config/Code/User/settings.json"
+  VSCODE_DIR="$HOME/.config/Code/User"
+fi
+
+mkdir -p "$VSCODE_DIR"
+ln -sf "$ROOT/defaults/vscode.json" "$VSCODE_DIR/settings.json"
+
+# iTerm 2 Theme
+# https://iterm2.com/python-api/tutorial/running.html#auto-run-scripts
+
+if ( is_osx ); then
+  ITERM_DIR="$HOME/Library/ApplicationSupport/iTerm2/Scripts/AutoLaunch"
+  mkdir -p "$ITERM_DIR"
+  ln -sf "$ROOT/defaults/iterm2-theme.py" "$ITERM_DIR/theme.py"
 fi
