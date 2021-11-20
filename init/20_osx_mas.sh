8eb3d2e13d3b4b22c12c139ebb2bc6beacbd2a6e
Jan Michael Auer <mail@jauer.org>
2021-11-20 10:54:30 +0100

    Move AppStore installs last and add "Homie"

--- init/20_osx_mas.sh
+++
@@ -1,4 +0,0 @@
-is_osx || return 0
-
-mas install 497799835 # XCode
-mas install 1295203466 # Microsoft Remote Desktop
---
+++ init/50_osx_mas.sh
@@ -0,0 +1,5 @@
+is_osx || return 0
+
+mas install 1295203466 # Microsoft Remote Desktop
+mas install 1533590432 # Homie — Menu Bar App for HomeKit
+mas install 497799835  # XCode
