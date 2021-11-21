be07a9135efffc85e5abdcd37c0ac4dd91d71c49
Jan Michael Auer <mail@jauer.org>
2021-11-21 17:33:02 +0100

    Add Meeter MAS App

--- init/50_osx_mas.sh
+++ init/50_osx_mas.sh
@@ -2,4 +2,5 @@ is_osx || return 0
 
 mas install 1295203466 # Microsoft Remote Desktop
 mas install 1533590432 # Homie — Menu Bar App for HomeKit
+mas install 1510445899 # Meeter
 mas install 497799835  # XCode
