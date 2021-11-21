6a3b52001c7b1d04f966f3a2e69f19f640ef4431
Jan Michael Auer <mail@jauer.org>
2021-11-21 16:58:42 +0100

    Add initializer for macOS Quick Actions

--- init/40_defaults.sh
+++ init/40_defaults.sh
@@ -1,8 +1,13 @@
-# iTerm 2 Theme
-# https://iterm2.com/python-api/tutorial/running.html#auto-run-scripts
-
 if ( is_osx ); then
+  # Switch iTerm 2 Theme between light and dark
+  # https://iterm2.com/python-api/tutorial/running.html#auto-run-scripts
   ITERM_DIR="$HOME/Library/Application Support/iTerm2/Scripts/AutoLaunch"
   mkdir -p "$ITERM_DIR"
   ln -sf "$ROOT/defaults/iterm2-theme.py" "$ITERM_DIR/theme.py"
+
+  # Copy automator quick actions
+  SERVICES_DIR="$HOME/Library/Services/"
+  for workflow in $ROOT/defaults/*.workflow; do
+    ln -sf "$workflow" $SERVICES_DIR
+  done
 fi
