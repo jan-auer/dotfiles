f817d53a72b0076cb33cc16751bdcd2c1caa1569
Jan Michael Auer <mail@jauer.org>
2021-11-21 12:22:04 +0100

    Fix path for iTerm2 auto launch script

--- init/40_defaults.sh
+++ init/40_defaults.sh
@@ -2,7 +2,7 @@
 # https://iterm2.com/python-api/tutorial/running.html#auto-run-scripts
 
 if ( is_osx ); then
-  ITERM_DIR="$HOME/Library/ApplicationSupport/iTerm2/Scripts/AutoLaunch"
+  ITERM_DIR="$HOME/Library/Application Support/iTerm2/Scripts/AutoLaunch"
   mkdir -p "$ITERM_DIR"
   ln -sf "$ROOT/defaults/iterm2-theme.py" "$ITERM_DIR/theme.py"
 fi
