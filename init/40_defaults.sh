0799829651514bc127f91dcad351c9f385a8db75
Jan Michael Auer <mail@jauer.org>
2023-03-13 10:55:18 +0100

    Add a Rectangle config default

--- init/40_defaults.sh
+++ init/40_defaults.sh
@@ -10,4 +10,9 @@ if ( is_osx ); then
   for workflow in $ROOT/defaults/*.workflow; do
     ln -sf "$workflow" $SERVICES_DIR
   done
+
+  # Copy Rectangle config to application support
+  RECTANGLE_DIR="$HOME/Library/Application Support/Rectangle"
+  mkdir -p "$RECTANGLE_DIR"
+  ln -sf "$ROOT/defaults/RectangleConfig.json" "$RECTANGLE_DIR/RectangleConfig.json"
 fi
