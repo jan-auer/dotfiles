d0c8a940d9918b83ab8ff34824a70e0ea9617353
Jan Michael Auer <mail@jauer.org>
2024-06-25 11:52:34 +0200

    Remove iTerm theme script, this is now native in iTerm

--- init/40_defaults.sh
+++ init/40_defaults.sh
@@ -1,10 +1,4 @@
 if ( is_osx ); then
-  # Switch iTerm 2 Theme between light and dark
-  # https://iterm2.com/python-api/tutorial/running.html#auto-run-scripts
-  ITERM_DIR="$HOME/Library/Application Support/iTerm2/Scripts/AutoLaunch"
-  mkdir -p "$ITERM_DIR"
-  ln -sf "$ROOT/defaults/iterm2-theme.py" "$ITERM_DIR/theme.py"
-
   # Copy automator quick actions
   SERVICES_DIR="$HOME/Library/Services/"
   for workflow in $ROOT/defaults/*.workflow; do
