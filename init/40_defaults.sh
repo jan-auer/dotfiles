3b02cdc0e8c183c10df5f6729adad139aa9b0a8a
Jan Michael Auer <mail@jauer.org>
2024-06-25 11:55:04 +0200

    Remove Rectangle config defaults

--- init/40_defaults.sh
+++
@@ -1,6 +0,0 @@
-if ( is_osx ); then
-  # Copy Rectangle config to application support
-  RECTANGLE_DIR="$HOME/Library/Application Support/Rectangle"
-  mkdir -p "$RECTANGLE_DIR"
-  ln -sf "$ROOT/defaults/RectangleConfig.json" "$RECTANGLE_DIR/RectangleConfig.json"
-fi
