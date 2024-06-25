8529c8f027e49119fbcfb3b22d7b336f7bc78055
Jan Michael Auer <mail@jauer.org>
2024-06-25 11:53:41 +0200

    Remove workflows replaced by Raycast

--- init/40_defaults.sh
+++ init/40_defaults.sh
@@ -1,10 +1,4 @@
 if ( is_osx ); then
-  # Copy automator quick actions
-  SERVICES_DIR="$HOME/Library/Services/"
-  for workflow in $ROOT/defaults/*.workflow; do
-    ln -sf "$workflow" $SERVICES_DIR
-  done
-
   # Copy Rectangle config to application support
   RECTANGLE_DIR="$HOME/Library/Application Support/Rectangle"
   mkdir -p "$RECTANGLE_DIR"
